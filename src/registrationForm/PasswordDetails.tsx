b48b79b4c323c18eb36d22b1ae9251a70a43b847
Denys Markevych <denysjse@gmail.com>
2023-04-27 08:11:43 +0300

    Registration Form Add an Error Style for Input

--- src/registrationForm/PasswordDetails.tsx
+++ src/registrationForm/PasswordDetails.tsx
@@ -8,9 +8,11 @@ type PasswordData = {
 
 type PasswordFormProps = PasswordData & {
   updateFields: (fields: Partial<PasswordData>) => void
+  passwordsMatch: boolean;
+  setPasswordsMatch: (match: boolean) => void;
 }
 
-export function PasswordDetails({ password, confirmPassword, updateFields}: PasswordFormProps) {
+export function PasswordDetails({ password, confirmPassword, updateFields, passwordsMatch}: PasswordFormProps) {
   const [showPassword, setShowPassword] = useState(false);
   return (
     <FormWrapper>
@@ -19,15 +21,15 @@ export function PasswordDetails({ password, confirmPassword, updateFields}: Pass
             type={showPassword ? 'text' : 'password'}  
             value={password} 
             onChange={e => updateFields({ password: e.target.value })}  
-            className="form--input"/>
-      <label className="form--label">Password</label>
+            className={`form--input ${passwordsMatch ? '' : 'confirm--password--error'}`}/>
+      <label className={`form--label ${passwordsMatch ? '' : 'form--label--error'}`}>{passwordsMatch ? 'Password' : 'Password do not match'}</label>
       <input
             required 
             type={showPassword ? 'text' : 'password'}
             value={confirmPassword} 
             onChange={e => updateFields({ confirmPassword: e.target.value })}  
-            className="form--input"/>
-      <label className="form--label second--label">Confirm</label>
+            className={`form--input ${passwordsMatch ? '' : 'confirm--password--error'}`}/>
+      <label className={`form--label second--label ${passwordsMatch ? '' : 'form--label--error'}`}>{passwordsMatch ? 'Confirm Password' : 'Password do not match'}</label>
       <div className="checkbox">
         <input type="checkbox" className="checkbox--input" onChange={() => setShowPassword(!showPassword)}/>
         <p className="show--password">Show password</p>
--- src/registrationForm/PhoneDetails.tsx
+++ src/registrationForm/PhoneDetails.tsx
@@ -12,21 +12,40 @@ type PhoneFormProps = PhoneData & {
 }
 
 export function PhoneDetails({ phoneNumber, updateFields, setIsValid}: PhoneFormProps) {
-  const [ , setInputValue] = useState("");
+  const [ inputValue, setInputValue] = useState("");
+  const allowedPrefixes = ['039', '050', '063', '066', '067', '068', '091', '092', '093', '094', '095', '096', '097', '098', '099'];
+  const [inputClassName, setInputClassName] = useState("");
 
   const handlePhoneNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     setInputValue(value);
-    const valid = value.length === 12;
-    setIsValid(valid);
-    updateFields({ phoneNumber: event.target.value });
+    const isValidLength = value.length === 12;
+    const isValidPrefix = allowedPrefixes.some(prefix => value.startsWith(prefix));
+    setIsValid(isValidLength && isValidPrefix);
+    if (isValidLength && isValidPrefix) {
+      updateFields({ phoneNumber: value });
+    }
+    if (value.length === 12) {
+      setInputClassName(isValidPhoneNumber(value) ? "phone--input-valid" : "phone--input-invalid");
+    } else {
+      setInputClassName("");
+    }
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter" && event.currentTarget.value.length !== 12) {
-      event.preventDefault();
+    if (event.key === "Enter") {
+      const value = event.currentTarget.value;
+      const validPrefix = allowedPrefixes.some(prefix => value.startsWith(prefix));
+      if (!validPrefix || value.length !== 12) {
+        event.preventDefault();
+      }
     }
   };
+
+  let label = "Phone number";
+  if (inputValue.length === 12 && !isValidPhoneNumber(inputValue)) {
+    label = "Incorrect Phone Number";
+  }
 
   return (
     <FormWrapper>
@@ -40,9 +59,17 @@ export function PhoneDetails({ phoneNumber, updateFields, setIsValid}: PhoneForm
         value={phoneNumber}
         onChange={handlePhoneNumberChange}
         onKeyDown={handleKeyDown}
-        className="form--input"
+        className={`phone--input ${inputClassName}`}
       />
-      <label className="form--label">Phone number</label>
+      <label className="form--label">{label}</label>
+      <span className="country--code">+38</span>
     </FormWrapper>
   )
+}
+
+function isValidPhoneNumber(phoneNumber: string) {
+  const allowedPrefixes = ['039', '050', '063', '066', '067', '068', '091', '092', '093', '094', '095', '096', '097', '098', '099'];
+  const validLength = phoneNumber.length === 12;
+  const validPrefix = allowedPrefixes.some(prefix => phoneNumber.startsWith(prefix));
+  return validLength && validPrefix;
 }
